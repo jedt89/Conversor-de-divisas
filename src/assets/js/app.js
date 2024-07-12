edd31f7dc9cac6e7b69a9a4e8b2009c28b4487b9
Jona <jonathan.diaz@driv.in>
2024-07-11 20:21:13 -0400

    update responsive design

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,14 +8,15 @@ const currencyName = document.querySelector('.currency-name');
 const currencyValue = document.querySelector('.currency-value');
 const currencyUpdate = document.querySelector('.currency-update');
 const totalContainer = document.querySelector('.total');
-const totalSection = document.querySelector('.total-section');
-const canvasContainer = document.getElementById('indicator-chart').getContext('2d');
+const canvasContainer = document
+  .getElementById('indicator-chart')
+  .getContext('2d');
 
 let currentIndicator = {};
 let currentMount = 0;
 
 const init = async () => {
-  const indicators = await getIndicators(null);
+  const indicators = await getIndicators();
   renderIndicators(indicators);
   setListeners(indicators);
   hideInfo();
@@ -25,12 +26,9 @@ const getIndicators = async (indicator) => {
   try {
     const response = await fetch(`${BASE_API}${indicator || ''}`);
     const data = await response.json();
-
-    if (!indicator) {
-      return Object.values(data).filter(item => typeof item == 'object');
-    } else {
-      return data.serie || [];
-    }
+    return indicator
+      ? data.serie || []
+      : Object.values(data).filter((item) => typeof item === 'object');
   } catch (error) {
     console.debug('Fetch error', error);
     return [];
@@ -48,95 +46,86 @@ const setListeners = (indicators) => {
   currencySelect.addEventListener('change', async (event) => {
     const selectedCode = event.target.value;
     if (selectedCode != '') {
-      currentIndicator = indicators.find(ind => ind.codigo === selectedCode);
-
+      currentIndicator = indicators.find((ind) => ind.codigo === selectedCode);
       if (currentIndicator) {
-        const dateConfig = {
-          weekday: 'long',
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric'
-        };
-        const { valor, nombre, fecha } = currentIndicator;
-        currencyName.textContent = nombre;
-        currencyValue.textContent = valor.toLocaleString('es-CL');
-        currencyUpdate.textContent = `Fecha de actualización: ${new Date(fecha).toLocaleDateString('es-ES', dateConfig)}`;
-        const indicatorHistory = await getIndicators(selectedCode);
-        const mappedIndicators = indicatorHistory.map(({ fecha, valor }) => ({
-          fecha: new Date(fecha).toLocaleDateString(),
-          valor
-        }));
-        generateChart(mappedIndicators.slice(-10));
-        showInfo();
-        totalContainer.textContent = 0
-        clpInput.value = ''
+        updateCurrencyInfo();
       }
     } else {
       hideInfo();
     }
   });
-
   clpInput.addEventListener('change', (event) => {
     currentMount = parseFloat(event.target.value);
   });
+  conversionButton.addEventListener('click', convertCurrency);
+};
 
-  conversionButton.addEventListener('click', () => {
-    convertCurrency();
-  });
+const updateCurrencyInfo = async () => {
+  const { valor, nombre, fecha } = currentIndicator;
+  currencyName.textContent = nombre;
+  currencyValue.textContent = `$${valor.toLocaleString('es-CL')}`;
+  currencyUpdate.textContent = `Fecha de actualización: ${new Date(
+    fecha
+  ).toLocaleDateString('es-ES', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })}`;
+  const indicatorHistory = await getIndicators(currentIndicator.codigo);
+  generateChart(
+    indicatorHistory.slice(-10).map(({ fecha, valor }) => ({
+      fecha: new Date(fecha).toLocaleDateString(),
+      valor
+    }))
+  );
+  showInfo();
+  totalContainer.textContent = '$0';
+  clpInput.value = '';
 };
 
 const convertCurrency = () => {
-  const result = currentIndicator.valor * currentMount;
-  totalContainer.textContent = result.toLocaleString('es-CL');
+  if (clpInput.value != '') {
+    const result = currentIndicator.valor * currentMount;
+    totalContainer.textContent = `$${result.toLocaleString('es-CL')}`;
+  }
 };
 
 const generateChart = (history) => {
   history.reverse();
-  const labels = history.map(item => item.fecha);
-  const data = history.map(item => parseFloat(item.valor.toFixed(0)));
+  const labels = history.map((item) => item.fecha);
+  const data = history.map((item) => parseFloat(item.valor.toFixed(0)));
   const config = {
     type: 'line',
     data: {
-      labels: labels,
-      datasets: [{
-        label: 'Valores de los últimos 10 días',
-        data: data,
-        pointRadius: 7,
-        borderColor: 'white',
-        borderWidth: 1,
-        pointBackgroundColor: 'orange',
-        pointBorderColor: 'transparent',
-        datalabels: {
-          color: 'white'
+      labels,
+      datasets: [
+        {
+          label: 'Valores de los últimos 10 días',
+          data,
+          pointRadius: 7,
+          borderColor: 'white',
+          borderWidth: 1,
+          pointBackgroundColor: 'orange',
+          pointBorderColor: 'transparent',
+          datalabels: {
+            color: 'white'
+          }
         }
-      }]
+      ]
     },
     options: {
       scales: {
-        y: {
-          ticks: {
-            color: 'white'
-          }
-        },
-        x: {
-          ticks: {
-            color: 'white' 
-          }
-        }
+        y: { ticks: { color: 'white' } },
+        x: { ticks: { color: 'white' } }
       },
-      plugins: {
-        legend: {
-          labels: {
-            color: 'white'
-          }
-        }
-      }
+      plugins: { legend: { labels: { color: 'white' } } }
     }
   };
 
-
-  if (Chart.getChart(canvasContainer)) {
-    Chart.getChart(canvasContainer).destroy();
+  const existingChart = Chart.getChart(canvasContainer);
+  if (existingChart) {
+    existingChart.destroy();
   }
   new Chart(canvasContainer, config);
 };
@@ -144,13 +133,11 @@ const generateChart = (history) => {
 const hideInfo = () => {
   currencyInfo.style.display = 'none';
   currencyUpdate.style.display = 'none';
-  totalSection.style.display = 'none';
 };
 
 const showInfo = () => {
   currencyInfo.style.display = 'flex';
   currencyUpdate.style.display = 'flex';
-  totalSection.style.display = 'flex';
 };
 
 init();
